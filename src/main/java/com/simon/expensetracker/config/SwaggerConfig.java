fccd259a94b0a37636ce5391f96fce10911020c6
iamveso <akpoveso.s@gmail.com>
2025-09-15 00:59:58 +0100

    attempting to solve swagger issue

--- src/main/java/com/simon/expensetracker/config/SwaggerConfig.java
+++ src/main/java/com/simon/expensetracker/config/SwaggerConfig.java
@@ -6,6 +6,10 @@ import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -24,6 +28,10 @@ public class SwaggerConfig {
                                 .email("simonakpoveso@yahoo.co.uk")
                         )
                 )
+                .servers(List.of(
+                        new Server().url("https://expensetracker-silent-frog-2188.fly.dev").description("Fly.io Deployment"),
+                        new Server().url("http://localhost:8080").description("Local Dev")
+                ))
                 .components(new Components()
                         .addSecuritySchemes("bearerAuth",
                                 new SecurityScheme()
